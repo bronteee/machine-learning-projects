3208d54a27d611575b6b64bc2b6134c5568bd16a
bronte-audere <bronte@auderenow.org>
2023-02-12 10:27:52 -0800

    Add colors and initial data visualization

---
+++ mlprojects/project3/utility/colors.py
@@ -0,0 +1,15 @@
+# Pretty colors
+CB91_Blue = '#2CBDFE'
+CB91_Green = '#47DBCD'
+CB91_Pink = '#F3A0F2'
+CB91_Purple = '#9D2EC5'
+CB91_Violet = '#661D98'
+CB91_Amber = '#F5B14C'
+color_list1 = [CB91_Blue, CB91_Pink, CB91_Green, CB91_Amber, CB91_Purple, CB91_Violet]
+color_list2 = [
+    '1E4052',
+    '51495B',
+    'E66954',
+    'EE7222',
+    'F29725',
+]
